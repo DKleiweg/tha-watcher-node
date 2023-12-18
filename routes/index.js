5fda9d586d612a367353ad9eaa9a0ee297af5685
Damien Kleiweg <d.kleiweg@flooris.nl>
2023-12-18 20:40:22 +0100

    Added axios

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
+const axios = require('axios');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/make-api-call', async (req, res) => {
+  try {
+    // Make an API call using axios
+    const response = await axios.get('https://dummyjson.com/products');
+
+    // Handle the response data
+    const responseData = response.data;
+    res.json(responseData);
+  } catch (error) {
+    // Handle errors
+    console.error('Error making API call:', error.message);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
 
 module.exports = router;
